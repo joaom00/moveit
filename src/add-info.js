a0f6d687040e3af95c523679c7524882ccc0f3c5
joaom <joaomljcanal@gmail.com>
2021-03-03 23:53:24 -0300

    Update

--- src/add-info.js
+++ src/add-info.js
@@ -8,13 +8,9 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   localStorage.setItem('@MoveIt:name', inputName.value);
   localStorage.setItem('@MoveIt:img', inputLink.value);
-  if (window.location.origin === 'https://joaom00.github.io') {
-    window.location.pathname = '/MoveIt';
-  }
+  window.location.pathname = '/MoveIt';
 });
 
 notAddButon.addEventListener('click', () => {
-  if (window.location.origin === 'https://joaom00.github.io') {
-    window.location.pathname = '/MoveIt';
-  }
+  window.location.pathname = '/MoveIt';
 });
