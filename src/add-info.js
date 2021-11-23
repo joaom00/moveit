21e167bb7d121b3194311b1bc3f63a9ab34a403a
João Pedro <joaopsilvamagalhaes@gmail.com>
2021-11-23 18:24:32 -0300

    update

--- src/add-info.js
+++ src/add-info.js
@@ -1,23 +1,23 @@
-const notAddButon = document.querySelector('.not-add-button');
-const form = document.querySelector('form');
-const inputName = document.querySelector('.input-name');
-const inputLink = document.querySelector('.input-link');
+const notAddButon = document.querySelector(".not-add-button");
+const form = document.querySelector("form");
+const inputName = document.querySelector(".input-name");
+const inputLink = document.querySelector(".input-link");
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  localStorage.setItem('@MoveIt:name', inputName.value);
-  localStorage.setItem('@MoveIt:img', inputLink.value);
-  if (window.location.origin === 'https://joaom00.github.io') {
-    window.location.href = 'https://joaom00.github.io/MoveIt/';
+  localStorage.setItem("@MoveIt:name", inputName.value);
+  localStorage.setItem("@MoveIt:img", inputLink.value);
+  if (window.location.origin === "https://joaom00.github.io") {
+    window.location.href = "https://joaom00.github.io/moveit/";
   } else {
-    window.location.pathname = '/index.html';
+    window.location.pathname = "/index.html";
   }
 });
 
-notAddButon.addEventListener('click', () => {
-  if (window.location.origin === 'https://joaom00.github.io') {
-    window.location.href = 'https://joaom00.github.io/MoveIt/';
+notAddButon.addEventListener("click", () => {
+  if (window.location.origin === "https://joaom00.github.io") {
+    window.location.href = "https://joaom00.github.io/moveit/";
   } else {
-    window.location.pathname = '/index.html';
+    window.location.pathname = "/index.html";
   }
 });
